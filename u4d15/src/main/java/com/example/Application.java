309b37cd7df00978860121d1ea528c8edf478978
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-08-23 10:15:48 +0200

    initial setting

---
+++ u4d15/src/main/java/com/example/Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4d15");
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
